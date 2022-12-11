6d2eda562fae8fe7398736504ac962fc1846c0b8
Raja Osama <therajaosama08@gmail.com>
2022-12-11 11:06:44 +0000

    add some comments

--- applied_assignment2.py
+++ applied_assignment2.py
@@ -55,7 +55,17 @@ def data_frame(data, indicator):
 # Line plot accepts file, indicator, image name
 
 
-def line_plot(data, indicator_name, file_name):
+def line_plot(data, indicator_name, image_name):
+    """
+    Produces a line plot.
+    data: list of data values
+    incdicator: a constant value contain name of indicator
+    image_name: name of plot image
+
+    this function is used to take three arguments. first filter the data on based on countries name
+    then match the data with a constant variable named indicator. When data is match further we plot
+    the data. Also, save the plot image before return
+    """
     # set figure size
     plt.figure(dpi=144)
 
@@ -68,14 +78,27 @@ def line_plot(data, indicator_name, file_name):
     # setting lable on y axis
     plt.ylabel('Data Growth')
     plt.legend(loc='best', bbox_to_anchor=(1, 0.5))
-    plt.savefig(file_name, bbox_inches='tight')
+    # take constant file name. second argumend is used to remove extra spaces in image
+    plt.savefig(image_name, bbox_inches='tight')
     return plt.show()
 
 
-# Bar chart accepts file, indicator, image name
+# Bar chart accepts file, y-axis label, indicator, image name
 
 
-def bar_chart(data, indicator_name, file_name):
+def bar_chart(data, indicator_name, y_label, image_name):
+    """
+    Produces a Bar Chart.
+    data: list of data values
+    incdicator: a constant value contain name of indicator
+    y_label: a constant value contain name of label
+    file_name: name of plot image
+
+    this function is used to take four arguments. first filter the data on based on countries name
+    then match the data with a constant variable named indicator.Third argument is a constant
+    name of label y-axis. In last, when data is match further we plot the bar chat of data.
+    Also, save the plot image before return
+    """
     data = data.loc[data['Years'].isin(
         ['1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005'])]
 
@@ -92,13 +115,25 @@ def bar_chart(data, indicator_name, file_name):
 
     plt.xticks(num, years)  # This is for showing years in x asis
     plt.xlabel('Years')
-    plt.ylabel('Total population')
+    plt.ylabel(y_label)
     plt.legend(loc='best', bbox_to_anchor=(1, 0.5))
-    plt.savefig(file_name, bbox_inches='tight')
+    # take constant file name. second argumend is used to remove extra spaces in image
+    plt.savefig(image_name, bbox_inches='tight')
     return plt.show()
 
 
-def heat_map(data, country):
+def heat_map(data, country, image_name):
+    """
+    Produces a Heat Map.
+    data: list of data values
+    country: a constant variable contain country name
+    file_name: name of plot image
+
+    this function is used to take three arguments. first filter the data on based on indicators
+    then match the data with a constant variable of countries.Third argument is a constant
+    name of heat map image. After ploting the heat map correlation matrix. In last, we save the
+    heat map image before return
+    """
     data = data.drop(['Country Code', 'Indicator Name'], axis=1)
     # create data frame
     new_df = pd.DataFrame()
@@ -140,7 +175,6 @@ def heat_map(data, country):
         ['Country Name', 'Indicator Code'], axis=1).T
 
     Indicator_data = Indicator_data.dropna().T
-
     new_df['CO2 emissions (kt)'] = Indicator_data.iloc[0]
 
     ax = plt.axes()
@@ -161,7 +195,9 @@ def heat_map(data, country):
                 linecolor='black',
                 linewidths=0.5,
                 ax=ax)
-    plt.show()
+    # take constant file name. second argumend is used to remove extra spaces in image
+    plt.savefig(image_name, bbox_inches='tight')
+    return plt.show()
 
 
 # line chart 1
@@ -184,17 +220,17 @@ df_countries, df_years, df_data = data_frame(
     'climate_data.csv', 'EG.ELC.COAL.ZS')
 # electricity form coal
 bar_chart(df_years, 'Electricity production from coal sources (% of total)',
-          'barplot_coal.png')
+          'Total population', 'barplot_coal.png')
 
 # passing file name and indicator in data_frame function
 df_countries, df_years, df_data = data_frame(
     'climate_data.csv', 'EN.ATM.CO2E.KT')
-# CO2 emissions (kt)
-bar_chart(df_years, 'CO2 emissions (kt)', 'barplot_carbon.png')
+# passing file name, indicator and image name
+bar_chart(df_years, 'CO2 emissions (kt)',
+          'Carbon Growth', 'barplot_carbon.png')
 
 
-# passing file and country name
-heat_map(df_data, 'Pakistan')
-heat_map(df_data, 'United Kingdom')
-heat_map(df_data, 'China')
-heat_map(df_data, 'China')
+# passing file, country name and image name
+heat_map(df_data, 'Pakistan', 'heatmap_pak.png')
+heat_map(df_data, 'United Kingdom', 'heatmap_uk.png')
+heat_map(df_data, 'New Zealand', 'heatmap_nz.png')
