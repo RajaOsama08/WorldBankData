5beed56d9562955b107c888905b2e0d666c1ba62
Raja Osama <therajaosama08@gmail.com>
2022-12-11 09:49:05 +0000

    doc string added

--- applied_assignment2.py
+++ applied_assignment2.py
@@ -10,30 +10,46 @@ import pandas as pd
 # list of countries (global variable)
 countries = ['Pakistan', 'India', 'New Zealand', 'China', 'United Kingdom']
 
+
 def data_frame(data, indicator):
     """
     Produces a data frame.
     data: list of data values
     incdicator: data column
-    
-    this function is used to read the file using pandas. use to filter indicator and make  new 
-    data frames, in last this function returns three variable data frame countries,years 
-    and original form of data.
-    """
-    
-    df_worldBank = pd.read_csv(data, skiprows=(4), index_col=(False))  # read csv file
-    df_worldBank = df_worldBank.loc[:, ~df_worldBank.columns.str.contains(
-        'Unnamed')]  # remove unnamed column
 
-    df_countries = df_worldBank.loc[df_worldBank['Country Name'].isin(countries)] #
-    df_countries = df_worldBank.loc[df_worldBank['Indicator Code'].eq(indicator)]
+    this function is used to read the file using pandas. use to filter
+    indicator and make  new data frames, in last this function returns three
+    variable data frame countries,years and original form of data.
+    """
+    # read csv file
+    df_worldBank = pd.read_csv(data, skiprows=(
+        4), index_col=(False))
+    # removing unnecessary column which are unnamed
+    df_worldBank = df_worldBank.loc[:, ~df_worldBank.columns.str.contains(
+        'Unnamed')]
+
+    # .isin() is pandas method used to filter data frames
+    df_countries = df_worldBank.loc[df_worldBank['Country Name'].isin(
+        countries)]
+
+    # .eq is a wrapper use for comparison in dataframe objects with constants,
+    # below indicator is constant
+    df_countries = df_worldBank.loc[df_worldBank['Indicator Code'].eq(
+        indicator)]
+
+    # .melt is a another type of transpose in this all column remain same.
+    # Below Just change Years into rows
     df_years = df_countries.melt(id_vars=['Country Name', 'Country Code',
-                  'Indicator Name', 'Indicator Code'], var_name='Years')
-    # df_years = df_worldBank.drop(['Country Code'],axis=1)
-    del df_years['Country Code']  # Deleting column
+                                          'Indicator Name', 'Indicator Code'],
+                                 var_name='Years')
+    # usign del funtion drop out the column
+    del df_years['Country Code']
+
     df_years = df_years.pivot_table('value', ['Years', 'Indicator Name', 'Indicator Code'],
-                          'Country Name').reset_index()
+                                    'Country Name').reset_index()
     return df_countries, df_years, df_worldBank
 
+
+# call data_frame function
 df = data_frame('climate_data.csv', 'SP.URB.TOTL')
 print(df)
